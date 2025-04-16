27b7c64f17086964b20aa929a1d4c6a6c1439d36
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-04-15 20:26:43 -0400

    Update dockerpy

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,16 @@
 FROM python:3.12-slim
 
 COPY . /app
-
 WORKDIR /app
-RUN pip install -r requirements.txt
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+ENV AM_I_IN_A_DOCKER_CONTAINER 1
+
+COPY requirements.txt .
+
+RUN mkdir -p /app/logs
+RUN pip install --no-cache-dir -r requirements.txt
+VOLUME /app/logs
+CMD [ "python3","main.py"]
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 import time
 import requests
@@ -14,13 +15,17 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+AM_I_IN_A_DOCKER_CONTAINER = os.environ.get(
+    "AM_I_IN_A_DOCKER_CONTAINER", False)
+
 
 def main():
     r = requests.get("https://www.google.com")
     logger.info("Hello World")
     logger.info("Starting a complex calculation")
     time.sleep(5)
-    logger.info(r.text)
+    logger.info(r.status_code)
+    logger.info(AM_I_IN_A_DOCKER_CONTAINER)
     logger.info("37")
     logger.info("Completing a complex calculation")
 
